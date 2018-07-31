0b834db8e101f6e09ba20393e0419e0b634bd2d8
ronaldcard <ronald.a.card@gmail.com>
2018-07-31 19:28:52 -0400

    added tests & methods setBasePriceOfItem and getBasePriceOfItem

--- src/main/java/com/pillar/kata/checkoutordertotal/pricing/PricingService.java
+++ src/main/java/com/pillar/kata/checkoutordertotal/pricing/PricingService.java
@@ -1,14 +1,81 @@
 package com.pillar.kata.checkoutordertotal.pricing;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Service;
+
+import com.pillar.kata.checkoutordertotal.item.Item;
+import com.pillar.kata.checkoutordertotal.item.Price;
 
 /**
  * Pricing Service.
  * 
- * @author PatientZero
+ * @author tony.card
  */
 @Service
 public class PricingService {
 	
+	private Map<Item, Price> basePrices;
 	
+	/**
+	 * Initializes the item prices.
+	 */
+	@PostConstruct
+	public void initializeItemPrices() {
+		
+		this.basePrices = new HashMap<>();
+		
+		// potentially load from repository, if we plan to persist the inventory
+	}
+	
+	/**
+	 * Gets the base {@link Price} of an {@link Item} in the inventory.
+	 * 
+	 * @param item the item
+	 * @return the price
+	 */
+	public Price getBasePriceOfItem(final Item item) {
+		
+		if (this.basePrices.containsKey(item)) {
+			return this.basePrices.get(item);
+		}
+		
+		throw new PricingServiceException("No base price is configured for [" + item.getDescription() + "]");
+	}
+	
+	/**
+	 * Sets the base price of an {@link Item}.
+	 * 
+	 * @param item the item
+	 * @param price the price
+	 */
+	public void setBasePriceOfItem(final Item item, final Price price) {
+		
+		if (item == null || price == null) {
+			return;
+		}
+		
+		this.basePrices.put(item, price);
+	}
+
+	/**
+	 * Simple Getter.
+	 * 
+	 * @return the basePrices
+	 */
+	public Map<Item, Price> getBasePrices() {
+		return this.basePrices;
+	}
+
+	/**
+	 * Simple Getter.
+	 * 
+	 * @param basePrices the basePrices to set
+	 */
+	public void setBasePrices(final Map<Item, Price> basePrices) {
+		this.basePrices = basePrices;
+	}
 }
--- src/test/java/com/pillar/kata/checkoutordertotal/pricing/PricingServiceTests.java
+++ src/test/java/com/pillar/kata/checkoutordertotal/pricing/PricingServiceTests.java
@@ -1,12 +1,19 @@
 package com.pillar.kata.checkoutordertotal.pricing;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import java.math.BigDecimal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.pillar.kata.checkoutordertotal.item.Item;
+import com.pillar.kata.checkoutordertotal.item.Price;
+import com.pillar.kata.checkoutordertotal.item.Unit;
 
 /**
  * Tests for the {@link PricingService}.
@@ -20,8 +27,29 @@ public class PricingServiceTests {
 	@Autowired
 	private PricingService service;
 	
+	/**
+	 * Tests that the {@link PricingService} was injected properly.
+	 */
 	@Test
 	public void injectionOfPricingService_Test() {
 		assertNotNull(this.service);
+	}
+	
+	/**
+	 * Tests being able to set the base price of an item.
+	 */
+	@Test
+	public void setBasePriceOfItem_Test() {
+		
+		final Item item = new Item("can of soup");
+		final Price priceToSet = new Price(new BigDecimal("1.89"), Unit.EACH);
+		
+		this.service.setBasePriceOfItem(item, priceToSet);
+		
+		final Price priceOfItemInInventory = this.service.getBasePriceOfItem(new Item("can of soup"));
+		
+		assertNotNull(priceOfItemInInventory);
+		assertEquals(new BigDecimal("1.89"), priceOfItemInInventory.getAmount());
+		assertEquals(Unit.EACH, priceOfItemInInventory.getUnit());
 	}
 }
