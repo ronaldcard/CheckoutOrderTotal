a349beff57d737194158c026d5f0fbd62c827dd4
ronaldcard <ronald.a.card@gmail.com>
2018-07-31 18:22:57 -0400

    added ComponentScan; implemented CommandLineRunner

--- src/main/java/com/pillar/kata/checkoutordertotal/application/CheckoutOrderTotalApplication.java
+++ src/main/java/com/pillar/kata/checkoutordertotal/application/CheckoutOrderTotalApplication.java
@@ -1,12 +1,31 @@
 package com.pillar.kata.checkoutordertotal.application;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Spring Boot application runner.
+ * 
+ * @author tony.card
+ */
 @SpringBootApplication
-public class CheckoutOrderTotalApplication {
-
+@ComponentScan("com.pillar.kata.checkoutordertotal")
+public class CheckoutOrderTotalApplication implements CommandLineRunner {
+	
+	/**
+	 * Main.
+	 * 
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CheckoutOrderTotalApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// add any custom run configurations
+		
 	}
 }
