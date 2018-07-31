024adffd445674e544d96d60da068c5ab3c27ed6
ronaldcard <ronald.a.card@gmail.com>
2018-07-31 17:38:09 -0400

    base spring boot initializr
    initial revision -started with a simple spring boot initializr project.

---
+++ src/main/java/com/pillar/kata/CheckoutOrderTotal/CheckoutOrderTotalApplication.java
@@ -0,0 +1,12 @@
+package com.pillar.kata.CheckoutOrderTotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutOrderTotalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutOrderTotalApplication.class, args);
+	}
+}
