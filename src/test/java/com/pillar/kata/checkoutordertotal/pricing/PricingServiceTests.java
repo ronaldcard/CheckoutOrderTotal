799d2e98e02573d21713442e722f29489e9615c6
ronaldcard <ronald.a.card@gmail.com>
2018-07-31 20:34:54 -0400

    refactored the tests a little bit.  added another item.

--- src/test/java/com/pillar/kata/checkoutordertotal/pricing/PricingServiceTests.java
+++ src/test/java/com/pillar/kata/checkoutordertotal/pricing/PricingServiceTests.java
@@ -27,6 +27,11 @@ public class PricingServiceTests {
 	@Autowired
 	private PricingService service;
 	
+	// common test items
+	private Item canOfSoup = new Item("can of soup");
+	private Item groundBeef = new Item("80% lean ground beef");
+	private Item bananas = new Item("bananas");
+	
 	/**
 	 * Tests that the {@link PricingService} was injected properly.
 	 */
@@ -36,31 +41,36 @@ public class PricingServiceTests {
 	}
 	
 	/**
-	 * Tests being able to set the base price of an item.
+	 * Tests both setting & getting the base price.
 	 */
 	@Test
-	public void setBasePriceOfItem_Test() {
+	public void configureBasePriceOfItem_Test() {
 		
-		final Item canOfSoup = new Item("can of soup");
 		final Price canOfSoupPrice = new Price(new BigDecimal("1.89"), Unit.EACH);
-		
-		final Item groundBeef = new Item("80% lean ground beef");
 		final Price groundBeefPrice = new Price(new BigDecimal("5.99"), Unit.POUND);
+		final Price bananasPrice = new Price(new BigDecimal("2.38"), Unit.POUND);
 		
-		this.service.setBasePriceOfItem(canOfSoup, canOfSoupPrice);
-		this.service.setBasePriceOfItem(groundBeef, groundBeefPrice);
+		this.service.setBasePriceOfItem(this.canOfSoup, canOfSoupPrice);
+		this.service.setBasePriceOfItem(this.groundBeef, groundBeefPrice);
+		this.service.setBasePriceOfItem(this.bananas, bananasPrice);
 		
-		final Price canOfSoupInInventory = this.service.getBasePriceOfItem(new Item("can of soup"));
-		final Price goundBeefInInventory = this.service.getBasePriceOfItem(new Item("80% lean ground beef"));
+		final Price configuredCanOfSoupPrice = this.service.getBasePriceOfItem(new Item("can of soup"));
+		final Price configuredGoundBeefPrice = this.service.getBasePriceOfItem(new Item("80% lean ground beef"));
+		final Price configuredBananasPrice = this.service.getBasePriceOfItem(new Item("bananas"));
 		
 		// canOfSoup
-		assertNotNull(canOfSoupInInventory);
-		assertEquals(new BigDecimal("1.89"), canOfSoupInInventory.getAmount());
-		assertEquals(Unit.EACH, canOfSoupInInventory.getUnit());
+		assertNotNull(configuredCanOfSoupPrice);
+		assertEquals(new BigDecimal("1.89"), configuredCanOfSoupPrice.getAmount());
+		assertEquals(Unit.EACH, configuredCanOfSoupPrice.getUnit());
 		
 		// groundBeef
-		assertNotNull(goundBeefInInventory);
-		assertEquals(new BigDecimal("5.99"), goundBeefInInventory.getAmount());
-		assertEquals(Unit.POUND, goundBeefInInventory.getUnit());
+		assertNotNull(configuredGoundBeefPrice);
+		assertEquals(new BigDecimal("5.99"), configuredGoundBeefPrice.getAmount());
+		assertEquals(Unit.POUND, configuredGoundBeefPrice.getUnit());
+		
+		// bananas
+		assertNotNull(configuredBananasPrice);
+		assertEquals(new BigDecimal("2.38"), configuredBananasPrice.getAmount());
+		assertEquals(Unit.POUND, configuredBananasPrice.getUnit());
 	}
 }
