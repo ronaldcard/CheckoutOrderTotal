e0f72aa7c00fee2ae234b5714f1dd6d084af5f6b
ronaldcard <ronald.a.card@gmail.com>
2018-08-03 18:02:54 -0400

    created new bean and test

---
+++ src/main/java/com/pillar/kata/checkoutordertotal/register/Register.java
@@ -0,0 +1,14 @@
+package com.pillar.kata.checkoutordertotal.register;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Object representing the cash register.
+ * 
+ * @author tony.card
+ */
+@Component
+public class Register {
+	
+	
+}
